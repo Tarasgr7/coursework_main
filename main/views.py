26a4f072bc2272f65d2f1745768e9f9c6bb669e9
Tarasgr7 <114420340+Tarasgr7@users.noreply.github.com>
2024-11-06 21:41:12 +0200

    The main app is ready

--- main/views.py
+++ main/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 
 def index(request):
     context={
-        'title':'Home',
-        'content':'Головна сторінка'
+        'title':'Головна сторінка ',
+        'content':'Магазин меблів ',
     }
     return render(request,'main/index.html',context)
 def about(request):
-    return HttpResponse("Home space")
+    context={
+        'title':'Про нас ',
+        'content':'Про нас ',
+        'text_on_page':'Наш магазин чудовий',
+    }
+    return render(request,'main/about.html',context)
